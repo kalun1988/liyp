07d46a4068e994c40d9407d6a183b22064868ed9
Sun Lau <kalun1988@gmail.com>
2018-10-10 20:01:49 +0800

    implement multi language

---
+++ script/lp-i18n.js
@@ -0,0 +1,29 @@
+const i18n_json = `{
+	"common": {
+		"appName": ["Lab In Your Pocket", "隨身實驗室", "隨身實驗室"]
+	},
+	"page": {
+		"index": {
+			"buttons": {
+				"gasLaw": ["Gas Law", "Gas Law 繁中", "Gas Law 簡中"],
+				"em": ["Magnetic Field with Current Flow", "Magnetic Field with Current Flow 繁中", "Magnetic Field with Current Flow 簡中"]
+			},
+			"footer":{
+				"credit":[
+					"<p>Department of Applied Physics</p><p>Hong Kong Polytechnic University</p>",
+					"<p>香港理工大學應用物理系</p>",
+					"<p>香港理工大學應用物理系</p>"
+				]
+			}
+		},
+		"experiment":{
+			"gasLaw":{
+				
+			}
+		}
+	}
+}`;
+
+
+
+S.i18n=JSON.parse(i18n_json);
--- script/lp-main.js
+++ script/lp-main.js
@@ -1,26 +1,12 @@
 (function() {
     'use strict';
     // this function is strict...
-    window.S = {};
-    S.device = null;
-    S.sensor = {};
-    S.plot = null;
-    S.accumulatedDataLimit = 100;
-    S.accumulatedData = {
-        x: [],
-        y: [],
-        z: [],
-        m: [],
-        t: []
-    };
-    S.current = {};
-    S.current.x = 15;
-    if (Cookies.get('lang') === undefined || Cookies.get('lang') === null) {
-        Cookies.set('lang', 0, { expires: 9999 });  //0:en, 1:tc, 2:sc
-        S.lang=0;       //cannot set cookie when device not allow cookies e.g. cocoon debug mode
-    }else{
-        S.lang = Cookies.get('lang');
-    }
+    // if (Cookies.get('lang') === undefined || Cookies.get('lang') === null) {
+    //     Cookies.set('lang', 0, { expires: 9999 });  //0:en, 1:tc, 2:sc
+    //     S.lang=0;       //cannot set cookie when device not allow cookies e.g. cocoon debug mode
+    // }else{
+    //     S.lang = Cookies.get('lang');
+    // }
 
     if (window.cordova) {
         document.addEventListener("deviceready", main);
@@ -47,12 +33,5 @@
             },50);
         });
     }
-    //handle i18n on front end
-    $(document).ready(function() {
-        $(".i18n").each(function() {
-            var data = $(this).data('i18n').split("||");
-            $(this).html(data[S.lang]); //0:en, 1:tc, 2:sc
-        });
-    });
 
 }());
